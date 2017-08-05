019b362727feff4a140ee715b59bff8fb54b0ece
Eliel Parra <elielparra@gmail.com>
2017-08-04 23:33:36 -0400

    Added pseudo-login and visual aid

--- chores/chores/chores.py
+++ chores/chores/chores.py
@@ -6,10 +6,32 @@ app.config.from_object(__name__)
 
 app.config.update(dict(
     SECRET_KEY='ABC123',
-    USERNAME='eliel'
+    USERNAME='eliel',
+    USERS=['evie', 'gael'], # Move to DB
 ))
 app.config.from_envvar('CHORES_SETTINGS', silent=True)
 
 @app.route('/')
 def show_chores():
-    return render_template('show_chores.html')
+    return render_template('show_chores.html', usernames=app.config['USERS'])
+
+@app.route('/add', methods=['POST'])
+def add_chore():
+    if not session.get('logged_in'):
+        abort(401)
+    # Insert new entry into DB
+    # Code here...
+    flash('Nueva tarea agregada')
+    return redirect(url_for('show_chores'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form.get('user', None) not in app.config['USERS']:
+            error = 'Usuario no encontrado'
+        else:
+            session['active_user'] = request.form.get('user', None)
+            flash('Usuario actualizado')
+            return redirect(url_for('show_chores'))
+    return render_template('login.html', error=error)
