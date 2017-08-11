7a937c39dc5322d965b1736b6b2af53984d2721b
Eliel Parra <elielparra@gmail.com>
2017-08-11 18:53:38 -0400

    Organized project directory, few errors in controller

--- chores/chores/schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS chores;
-CREATE TABLE chores(
-    id integer PRIMARY KEY AUTOINCREMENT,
-    title text NOT NULL,
-    owner text NOT NULL,
-    status integer NOT NULL
-);
--- chores/config.py
+++ chores/config.py
@@ -1,6 +1,25 @@
-DATABASE='/home/eliel/Desarrollo/tutorials/chores/chores/chores.db' #Figure out how to use app.root_path (if worthy)
-SQLALCHEMY_DATABASE_URI='sqlite:////home/eliel/Desarrollo/tutorials/chores/chores/chores.db' #Figure out how to use app.root_path (of worthy)
-SQLALCHEMY_TRACK_MODIFICATIONS=False
-SECRET_KEY='ABC123'
-USERNAME='eliel'
-USERS=['evie', 'gael', 'admin']
+# Statements for enabling the development environment
+DEBUG = True
+
+# Define the application directory
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Define the database we are working with
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'chores.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+DATABASE_CONNECT_OPTIONS = {}
+
+# Applications threads
+THREADS_PER_PAGE = 2
+
+# Protection against CSRF
+CSRF_ENABLED = True
+CSRF_SESSION_KEY = "b71df400c0dce8799bc90b60f1f35baaf35fb83bd74589c0"
+
+# Secret key for signing cookies
+SECRET_KEY = "dacae7cdb437d289350c12e3726bb9ab4a751bc669b197ec"
+
+# For testing purposes
+USERNAME = 'eliel'
+USERS = ['evie', 'gael', 'admin']
