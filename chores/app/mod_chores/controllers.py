4054073586b4b7964f2a14770d674cf66ac54df6
Eliel Parra <elielparra@gmail.com>
2017-08-17 19:41:20 -0300

    Implemented WTForms and validation (No error messages)

--- chores/app/mod_chores/controllers.py
+++ chores/app/mod_chores/controllers.py
@@ -9,21 +9,27 @@ mod_chores = Blueprint('chores', __name__, url_prefix='/chores')
 @mod_chores.route('/')
 def show_chores():
 
-    form = LoginForm(request.form)
-    if session['active_user'] == 'admin':
-        chores = Chore.query.filter(Chore.status == 1).order_by(Chore.id.desc())
+    login_form = LoginForm(request.form)
+    chore_form = ChoreForm(request.form)
+    if 'active_user' in session:
+        if session['active_user'] == 'admin':
+            chores = Chore.query.filter(Chore.status == 1).order_by(Chore.id.desc())
+        else:
+            chores = Chore.query.filter(Chore.owner == session['active_user'], Chore.status == 1).order_by(Chore.id.desc())
     else:
-        chores = Chore.query.filter(Chore.owner == session['active_user'], Chore.status == 1).order_by(Chore.id.desc())
-    return render_template('mod_chores/show_chores.html', usernames=current_app.config['USERS'], chores=chores, form=form)
+        chores = []
+    return render_template('mod_chores/show_chores.html', chores=chores, login_form=login_form, chore_form=chore_form)
 
 @mod_chores.route('/add', methods=['POST'])
 def add_chore():
 
-    chore = Chore(request.form['chore'], request.form['owner'], 1)
-    db.session.add(chore)
-    db.session.commit()
-    flash('Nueva tarea agregada')
-    return redirect(url_for('show_chores'))
+    form = ChoreForm(request.form)
+    if request.method == 'POST' and form.validate():
+        chore = Chore(form.title.data, form.owner.data, 1)
+        db.session.add(chore)
+        db.session.commit()
+        flash('Nueva tarea agregada')
+    return redirect(url_for('chores.show_chores'))
 
 @mod_chores.route('/complete', methods=['POST'])
 def complete_chore():
@@ -32,7 +38,7 @@ def complete_chore():
     chore.status = 0
     db.session.commit()
     flash('Tarea completada')
-    return redirect(url_for('show_chores'))
+    return redirect(url_for('chores.show_chores'))
 
 @mod_chores.route('/remove', methods=['POST'])
 def delete_chore():
@@ -40,14 +46,14 @@ def delete_chore():
     Chore.query.filter(Chore.id == request.form['id']).delete()
     db.session.commit()
     flash('Tarea eliminada')
-    return redirect(url_for('show_chores'))
+    return redirect(url_for('chores.show_chores'))
 
 @mod_chores.route('/login', methods=['GET', 'POST'])
 def login():
     
     form = LoginForm(request.form)
-    if form.user.data in current_app.config['USERS']:
-        session['active_user'] = form.user.data
-        flash('Usuario actualizado')
-        return redirect(url_for('chores.show_chores'))
-    return render_template('mod_chores/partials/login.html', form=form)
+    if request.method == 'POST' and form.validate():
+        if form.user.data in current_app.config['USERS']:
+            session['active_user'] = form.user.data
+            flash('Usuario actualizado')
+    return redirect(url_for('chores.show_chores'))
--- chores/app/mod_chores/forms.py
+++ chores/app/mod_chores/forms.py
@@ -3,14 +3,27 @@ from flask_wtf import Form
 from wtforms import TextField, RadioField, HiddenField, SubmitField
 from wtforms.validators import Required
 
+from app import app
+
+# Helper function to build the list of users, choices for radio buttons (Might need to move this)
+def get_user_list(no_admin=True):
+
+    user_list = []
+
+    for user in app.config['USERS']:
+        if no_admin and user == 'admin': continue
+        user_list.append((user, user.capitalize()))
+
+    return user_list
+
 # Define the chore form
 class ChoreForm(Form):
 
     title = TextField('Nueva Tarea', [Required(message='Debe ingresar un titulo')])
-    owner = RadioField('Hijo', [Required(message='Debe seleccionar un hijo')], choices=[('gael', 'Gael'), ('evie', 'Evie')])
+    owner = RadioField('Hijo', [Required(message='Debe seleccionar un hijo')], choices=get_user_list())
 
 
 # Define the login form, this particular login doesn't use password
 class LoginForm(Form):
 
-    user = RadioField('Usuario', [Required(message='Debe seleccionar un hijo')], choices=[('gael', 'Gael'), ('evie', 'Evie')])
+    user = RadioField('Usuario', [Required(message='Debe seleccionar un hijo')], choices=get_user_list(no_admin=False))
